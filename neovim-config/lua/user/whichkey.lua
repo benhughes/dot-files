c8e44bf93150af0b8d6e1e0980fa2d72168042a1
ben <ben@thoughtmachine.net>
2022-12-14 12:00:44 +0000

    fixig spelling issue

--- neovim-config/lua/user/whichkey.lua
+++ neovim-config/lua/user/whichkey.lua
@@ -131,7 +131,7 @@ local mappings = {
       "Diff",
     },
     t = { 
-      "<cmd>Gitsigns toggle_deleted<cr><cmd>Gitsigns toggle_linehl<cr><cmd>GitSigns toggle_numhl<cr>",
+      "<cmd>Gitsigns toggle_deleted<cr><cmd>Gitsigns toggle_linehl<cr><cmd>Gitsigns toggle_numhl<cr>",
       "Toggle Git Diff Mode"
     },
   },
